781ba45d8251bfdd5caa7c51cb13ec289bfb7182
franchescamansour <cocomansour@Franchesca.local>
2016-03-01 13:45:50 -0500

    Code done

--- js/main.js
+++ js/main.js
@@ -1,52 +1,15 @@
-var answer = prompt('Enter a Letter');
 
-for (counter = 0; counter < 1; counter++) {
-  answer = answer;
-  document.write(answer + '<br>');
-}
+var letter = prompt('letter');
 
-for (counter = 1; counter < 2; counter++) {
-  answer = answer;
-  document.write(answer + answer + '<br>');
-}
+var i;
 
-for (counter = 2; counter < 3; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + '<br>');
-}
+var j;
 
-for (counter = 3; counter < 4; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + '<br>');
-}
+for (i = 0; i < 10; i++) {
 
-for (counter = 4; counter < 5; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + answer + '<br>');
-}
+  for(j = 0; j<= i; j++) {
+  document.write(letter);
+  }
 
-for (counter = 5; counter < 6; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + answer + answer + '<br>');
+  document.write('<br>');
 }
-
-for (counter = 6; counter < 7; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + answer + answer + answer + '<br>');
-}
-
-for (counter = 7; counter < 8; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + answer + answer + answer + answer +  '<br>');
-}
-
-for (counter = 8; counter < 9; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + answer + answer + answer + answer + answer + '<br>');
-}
-
-for (counter = 9; counter < 10; counter++) {
-  answer = answer;
-  document.write(answer + answer + answer + answer + answer + answer + answer + answer + answer + answer + '<br>');
-}
-Status API Training Shop Blog About Pricing
