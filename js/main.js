4fca70ebc31a65ef7ec31ff5ac9347a0015720e6
franchescamansour <cocomansour@Franchesca.local>
2016-03-01 14:10:07 -0500

    markbot

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var j;
 
 for (i = 0; i < 10; i++) {
 
-    for (j = 0; j <= i; j++) {
-    document.write(letter);
-    }
+  for (j = 0; j <= i; j++) {
+  document.write(letter);
+  }
 
   document.write('<br>');
 }
