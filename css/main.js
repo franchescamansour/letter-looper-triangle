5f56797c1cdba16e4305529b7ef175e9ae1a12ec
franchescamansour <cocomansour@Franchesca.local>
2016-03-01 13:39:56 -0500

    Testing

--- css/main.js
+++ css/main.js
@@ -4,3 +4,49 @@ for (counter = 0; counter < 1; counter++) {
   answer = answer;
   document.write(answer + '<br>');
 }
+
+for (counter = 1; counter < 2; counter++) {
+  answer = answer;
+  document.write(answer + answer + '<br>');
+}
+
+for (counter = 2; counter < 3; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + '<br>');
+}
+
+for (counter = 3; counter < 4; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + '<br>');
+}
+
+for (counter = 4; counter < 5; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + answer + '<br>');
+}
+
+for (counter = 5; counter < 6; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + answer + answer + '<br>');
+}
+
+for (counter = 6; counter < 7; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + answer + answer + answer + '<br>');
+}
+
+for (counter = 7; counter < 8; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + answer + answer + answer + answer +  '<br>');
+}
+
+for (counter = 8; counter < 9; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + answer + answer + answer + answer + answer + '<br>');
+}
+
+for (counter = 9; counter < 10; counter++) {
+  answer = answer;
+  document.write(answer + answer + answer + answer + answer + answer + answer + answer + answer + answer + '<br>');
+}
+Status API Training Shop Blog About Pricing
