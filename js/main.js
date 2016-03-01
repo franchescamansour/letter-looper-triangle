1b810eeae65262f0f3bfb693e9ca5ff51f549364
franchescamansour <cocomansour@Franchesca.local>
2016-03-01 14:09:37 -0500

    markbot

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ var i;
 var j;
 
 for (i = 0; i < 10; i++) {
-  for (j = 0; j <= i; j++) {
-  document.write(letter);
-  }
+
+    for (j = 0; j <= i; j++) {
+    document.write(letter);
+    }
 
   document.write('<br>');
 }
